99060defe528c1a917ef37effd9f535ee5c60bc8
SSYT 2.0 <SSYT@users.noreply.github.com>
2018-02-02 19:35:10 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -21,7 +21,7 @@
 	var index_chatbox = document.createElement('script');
 
 	fa_script_content.type = "text/javascript";
-	fa_script_content.innerHTML = '//<![CDATA[\n'+
+	fa_script_content.innerHTML = '<script type=\"text/javascript\">//<![CDATA[\n'+
 		'if(typeof(fa_script) == "undefined") var fa_script;\n'+
 		'window.fa_script = {\n'+
 			'  version: "1.0",\n'+
@@ -29,21 +29,17 @@
 			'  install: true,\n'+
 			'  forum: "'+ forum +'"\n'+
 		'};\n'+
-	'//]]>';
-
-	index_chatbox.type = "text/javascript";
-	index_chatbox.innerHTML = '//<![CDATA[\n'+
-		'document.write(\'<script type="text/javascript" src="http://www.cdn.faproject.eu/chatbox/fachatbox.php?forum='+ forum +'"><\/script>\');\n'+
-	'//]]>';
+	'//]]></script>';
+	
+	index_chatbox.innerHTML = '<script type="text/javascript">//<![CDATA[\n'+
+		'document.write(\'<script type="text/javascript" src="http://www.cdn.faproject.eu/chatbox/fachatbox.php?forum='+ forum +'"><\\/script>\');\n'+
+	'//]]></script>';
 
 
 	function installIndexBody() {
-		$.ajax({
-			url: index_body,
-			type: "GET",
-			succes: function(data) {
+		$.get(index_body).done(function(data) {
 				var tmp_val = $('textarea#template', data).val();
-				tmp_val = tmp_val.replace(/\{BOARD_INDEX\}/ig, "{BOARD_INDEX}" + index_chatbox);
+				tmp_val = tmp_val.replace(/\{BOARD_INDEX\}/ig, index_chatbox.innerHTML + "\n{BOARD_INDEX}");
 				$.post(index_body, {
           "template"	: tmp_val,
           "t"					: "116",
@@ -57,17 +53,13 @@
 						}, 2500);
 					});
 				});
-			}
 		});
 	};
 
 	$(document).on("click", "input[name=\"fa_install_NotFind\"]", function() {
-		$.ajax({
-			url: header,
-			type: "GET",
-			succes: function() {
+		$.get(header).done(function(data) {
 				var templates = $('textarea#template', data).val();
-				templates = templates.replace(/\{HOSTING_JS\}/ig, "{HOSTING_JS}" + fa_script_content);
+				templates = templates.replace(/\{HOSTING_JS\}/ig, "{HOSTING_JS}\n" + fa_script_content.innerHTML);
 				$.post(header, {
           "template"	: templates,
           "t"					: "110",
@@ -75,14 +67,10 @@
           "tpl_name"	: "index_body",
           "submit" 	: 1
 				}).done(function() {
-					$.post('/admin/index.forum?part=themes&sub=templates&mode=edit_main&main_mode=edit&extended_admin=1&t=110&l=main&pub=1&tid=' + tid).done(function() {
+					$.post('/admin/index.forum?part=themes&sub=templates&mode=edit_main&main_mode=edit&extended_admin=1&t=116&l=main&pub=1&tid=' + tid).done(function() {
 						installIndexBody();
 					});
 				});
-			},
-			error: function() {
-				alert("ERROR, refresh page and try again.");
-			}
 		});
 	});
 
