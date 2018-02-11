ba45ec1ce758c95af59f29aaca9d7ac03b68e97f
SSYT 2.0 <SSYT@users.noreply.github.com>
2018-02-11 15:30:50 +0100

    Update and rename server.inc.php to settings.config.php

---
+++ libs/settings.config.php
@@ -0,0 +1,11 @@
+<?php
+    /* 
+     * Database Connection Settings
+    */
+    $dbconfig = array(
+    	'driver' => 'mysql',
+    	'host' => '',
+    	'dbname' => '',
+    	'username' => '',
+    	'password' => ';
+?>
